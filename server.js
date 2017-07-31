bb4618e3b4ba71ed5a43447c31ee2da3e95f37e9
Devin Dixon <devinisafoal@gmail.com>
2017-07-31 12:38:50 -0600

    update server js

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const path = require('path');
-const router = require('./server/router');
 const bodyParser = require('body-parser');
-
 const port = (process.env.PORT || 3001);
 const app = express();
 
+const router = require('./server/router');
 
 app.use(bodyParser.json());
-
 app.use('/api/v1', router);
+app.use(express.static('public'));
+app.get('/', (request, response) => response.sendFile(path.join(__dirname, './client/public/index.html')));
 
 app.listen(port, () => {
-  console.log(`etude server listening on port ${port}!`);
+  console.log(`Etude server listening on port ${port}!`);
 });
 
 module.exports = app;

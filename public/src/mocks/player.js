3bb61192bf3720ab22cea1339567d09b0f438fcc
Devin Dixon <devinisafoal@gmail.com>
2017-07-31 14:41:01 -0600

    still not sure why nothing is rendering

---
+++ public/src/mocks/player.js
@@ -0,0 +1,17 @@
+export const songs = [
+  {
+    url: 'Users/dev/Downloads/Kiss Me Thru The Phone Lyrics Soulja Boy.mp3',
+    cover: 'path/to/jpeg',
+    artist: {
+      name: 'Soulja Boy',
+      song: 'Kiss Me Thru The Phone'
+    }
+  },
+  {
+    url: 'Users/dev/Downloads/Simple Plan-Don\'t Wanna Think About You.mp3',
+    artist: {
+      name: 'Simple Plan',
+      song: 'Don\'t Wanna Think About You'
+    }
+  }
+];

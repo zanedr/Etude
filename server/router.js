1084136f3ab389288167ee0bc129fc84655caa55
Zane Russell <zaneroosa@gmail.com>
2017-07-31 10:35:11 -0600

    added patch route

--- server/router.js
+++ server/router.js
@@ -1,6 +1,3 @@
-const environment = process.env.NODE_ENV || 'production';
-const config = require('./knexfile.js')[environment];
-const database = require('knex')(config);
 
 const express = require('express');
 const router = express.Router();
@@ -9,6 +6,8 @@ const api = require('./api');
 router.get('/songs', api.getSongs);
 
 router.post('/songs', api.getSongs);
+
+router.patch('/songs', api.getSongs);
 
 router.delete('/songs', api.getSongs);
 

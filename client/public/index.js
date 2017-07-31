caafe93d3f0edd4b1d6a18eb9ed470904cef14e5
Devin Dixon <devinisafoal@gmail.com>
2017-07-31 14:41:31 -0600

    remove client folder

--- client/public/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './assets/styles/main.css';
-import App from './src/Components/App/App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
